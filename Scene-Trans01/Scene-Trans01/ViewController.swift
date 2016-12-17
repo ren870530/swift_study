79016b679f5fbc59ff6bddcc0ddb1fe47fa8bd3d
ren <ren@rens-MacBook-Air.local>
2016-12-17 22:54:52 +0900

    new project

---
+++ Scene-Trans01/Scene-Trans01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Scene-Trans01
+//
+//  Created by ren on 2016/12/12.
+//  Copyright © 2016 ren. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
